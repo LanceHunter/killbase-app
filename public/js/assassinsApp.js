02f3523fbda918775452b1c6a2dd2d1178489491
Lance Hunter <lancehunter@gmail.com>
2017-12-16 23:12:17 -0600

    Finished all assassin deletion functions.

--- public/js/assassinsApp.js
+++ public/js/assassinsApp.js
@@ -27,5 +27,32 @@
     });
   });
 
+  $('.deleteSolo').click(() => {
+    let clickedButtonID = event.target.id;
+    let assassin = event.target.id.slice(1);
+    $(`#${clickedButtonID}`).replaceWith(`<button class="button outline secondary round small realDelete" id="d<%= assassin.id %>">Confirm Delete</button>`);
+    $('.realDelete').click(() => {
+      $.ajax({
+        url: `/assassins/${assassin}`,
+        method : 'DELETE',
+        statusCode: {
+            404: function() {
+              window.alert('The page was not found');
+            },
+            500: function() {
+              window.alert('There was a server error');
+            }
+          }
+      })
+      .fail(function(err) {
+        window.alert('Something went wrong. Deletion not processed. Please try again later.');
+      })
+      .done(function(replytext) {
+        window.location.assign('/assassins');
+      });
+    });
+  });
+
+
 
 })();
